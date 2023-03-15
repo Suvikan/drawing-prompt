a7b32d26cd25e9a2be4750b6b8cd08febaa800f0
Suvikan <sanledell@gmail.com>
2023-03-15 14:45:33 +0200

    color palet works but nothing else

--- src/app/idea-prompt/idea-prompt.component.ts
+++ src/app/idea-prompt/idea-prompt.component.ts
@@ -11,7 +11,7 @@ import { DataService } from '../data.service';
 export class IdeaPromptComponent implements OnInit {
   ideasList: any = [];
 
-  constructor(private service: DataService) {}
+  constructor(private service: DataService) { }
 
   ngOnInit(): void {
     this.getIdeas();
